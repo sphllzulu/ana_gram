a40563beaeb54f075e948a80f7f27ce1d0472fb3
sphllzulu <117436803+sphllzulu@users.noreply.github.com>
2024-06-20 10:26:57 +0200

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,25 +1,23 @@
-//the objective is to firstly iterate through each character in the provided string
-//2nd objective is to ensure that the 2 strings are indeed an anagram
-
 function makeAnagram(str1, str2) {
   var obj = {};
 
+ //this will display the number of times each character appears for str1
   for (var i = 0; i < str1.length; i++) {
-    var character = str1[i]; //assign the character at a certain index to the variable 'character'
-    //checks if the character in a certain index already exists in the object 'obj', if it does then it is incremented by 1
-    if (obj[character[i]]) {
-      obj[character[i]]++;
+    var character = str1[i];
+    if (obj[character]) {
+      obj[character]++;
     } else {
-      //if the character in the specified index does not exist then the frequency in which the character appears will be =1
-      obj[character[i]] = 1;
+      obj[character] = 1;
     }
   }
 
+ //this will display the number of times each character appears
   for (var i = 0; i < str2.length; i++) {
     var character = str2[i];
-    //again checks if the
-    if (obj[character[i]]) {
-      obj[character[i]]--;
+    if (obj[character]) {
+      obj[character]++;
+    } else {
+      obj[character] = 1;
     }
   }
 
@@ -27,4 +25,6 @@ function makeAnagram(str1, str2) {
 }
 
 console.log(makeAnagram("cde", "dcf"));
-console.log(makeAnagram("cde", "abc"));
+console.log(makeAnagram("cde", "abc")); 
+
+
